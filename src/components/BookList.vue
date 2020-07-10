70145beb477e53c9d27afa9e1b712491a20e6865
Sébastien Langis <sebastien.langis@gmail.com>
2020-07-10 16:02:09 -0400

    Module 5

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -4,16 +4,19 @@
     <ul>
       <book-item v-for="book in books" :book="book"></book-item>
     </ul>
+    <book-form @addBook="appendBook"></book-form>
   </div>
 </template>
 
 <script>
 import BookItem from './BookItem';
+import BookForm from './BookForm';
 
 export default {
   name: 'BookList',
   components: {
     BookItem,
+    BookForm,
   },
   data() {
     return {
@@ -25,6 +28,14 @@ export default {
       ]
     };
   },
+  methods: {
+    appendBook(bookTitle, bookAuthor) {
+      this.books.push({
+        title: bookTitle,
+        author: bookAuthor,
+      });
+    }
+  }
 };
 </script>
 
